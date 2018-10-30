4fd80a934e3ee9a062e7ce7e47db921627796e25
Joseph Wildey <joewildey@outlook.com>
2018-10-30 19:19:41 -0400

    Update Utilitarianism.java
    Inspired by Professor Jianchu  Chen. A skeleton of a program that could use Utilitarianism to make decisions.

---
+++ Utilitarianism.java
@@ -0,0 +1,39 @@
+//This code is mostly useless and probably won't run.
+
+/*This would be where we collect the inputs
+and ultimately pass them through the objects.
+Just a hypothetical class, since I have no way
+of getting the required inputs in a solid way. */
+public class Utilitarianism {
+    public static void main(String[] args) {
+    }
+}
+
+
+class Bentham {
+
+    /* These are the values we could not necessarily get from the user.
+    However, they are still necessary for the calculation. We could just
+    get them from a database or something before going through with our
+    computation.*/
+    int cert, propin, fecund, purity, extent;
+    /*Intensity and duration could be probably collected
+    from the user so we would just pass that when we call the
+    class object.*/
+    public static String solveMath(int inten, int dur) {
+        String result = "";
+        return result;
+    }
+
+};
+
+class Mill {
+    /*This class would probably require a queue or
+    BST to store hobbies and educational levels. You could
+    pop off values off the queue until it matched a given string
+    of the hobby/sport/art, and base your score off the incrementation.
+    Otherwise, you could use a coded language to store each hobby/art/sport as a key in a BST
+    and its value in the value portion of the node.*/
+    /*You would also want a queue for stuff like educational level to determine who is the
+    most educated in a given context.*/
+};

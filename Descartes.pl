d75568741d32183124819d8dc2ceb44a87032471
Joseph Wildey <joewildey@outlook.com>
2018-11-02 04:21:55 -0400

    Update Descartes.pl
    Threw in more stuff to make the argument more complete or something I think.

--- Descartes.pl
+++ Descartes.pl
@@ -3,16 +3,21 @@ Along the way it turned into something resembling Aristolean philosophy. I think
 there is now some Thomas Aquinas stuff in here too. */
 
 /*First, we establish the facts. I think, I am human, and I exist. We must also establish the fact that God exists now. 
-Furthermore, by definition God is a perfect being.
+Furthermore, by definition God is a perfect being. Furthermore, I will also include the concept of perfect, which I believe
+Descartes said must have come from God or some such as proof that a evil genius could not be God or God could not be evil.
+Since only a being lesser than God would attempt to deceive him into believing God exists or something.
 Also, for funsies capitalize "God" or "Me." */
 exist(me).
 exist(god).
 human(me).
 think(me).
 perfectBeingExists(god).
+conceptOfPerfect(god).
 /*Then we set up the rule, I am a human if I exist and I think. */
 human(me):- think(me), exist(me).
-/* If I am a human then something greater than me must have created me. Therefore God must exist. */
-exist(god):- human(me).
+/* If I am a human then something greater than me must have created me, and since humans are not perfect
+and a deceptive higher being would still be imperfect, the concept of perfection must have come from
+God if it exists. Therefore God must exist. */
+exist(god):- human(me), conceptOfPerfect(god).
 /*If God exists then we must reason that a perfect being exists, because that is the definition of God. */
 perfectBeingExists(god):- exist(god).

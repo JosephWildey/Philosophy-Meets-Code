8982bd13fdd540de199c5f265ff568ecec5c9660
Joseph Wildey <joewildey@outlook.com>
2018-11-02 03:50:15 -0400

    Update Descartes.pl
    Made things a little more complex. We're going to dive deeper soon.

--- Descartes.pl
+++ Descartes.pl
@@ -1,7 +1,9 @@
-/*This is Descartes famous line, "I think therefore I am" expressed in Prolog. */
+/*This was Descartes famous line, "I think therefore I am" expressed in Prolog. 
+Along the way it turned into something resembling Aristolean philosophy. */
 
-/*First, we establish the facts. I think and I exist. */
+/*First, we establish the facts. I think, I am human, and I exist. */
 exist(me).
+human(me).
 think(me).
-/*Then we set up the rule, if it is true I think then it it is true I exist. */
-exist(me):- think(me).
+/*Then we set up the rule, I am a human if I exist and I think. */
+human(me):- think(me), exist(me).

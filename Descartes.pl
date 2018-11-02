c09a175b372224cf1a76a4150dfd25296861303d
Joseph Wildey <joewildey@outlook.com>
2018-11-02 04:01:56 -0400

    Update Descartes.pl
    Drove further into this for funsies.

--- Descartes.pl
+++ Descartes.pl
@@ -3,12 +3,16 @@ Along the way it turned into something resembling Aristolean philosophy. I think
 there is now some Thomas Aquinas stuff in here too. */
 
 /*First, we establish the facts. I think, I am human, and I exist. We must also establish the fact that God exists now. 
+Furthermore, by definition God is a perfect being.
 Also, for funsies capitalize "God" or "Me." */
 exist(me).
 exist(god).
 human(me).
 think(me).
+perfectBeingExists(god).
 /*Then we set up the rule, I am a human if I exist and I think. */
 human(me):- think(me), exist(me).
 /* If I am a human then something greater than me must have created me. Therefore God must exist. */
 exist(god):- human(me).
+/*If God exists then we must reason that a perfect being exists, because that is the definition of God. */
+perfectBeingExists(god):- exist(god).

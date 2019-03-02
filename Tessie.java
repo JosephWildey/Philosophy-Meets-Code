417aa2a94ce09fe6721d06fb6071532460d14470
Joseph Wildey <joewildey@outlook.com>
2019-03-02 02:16:23 -0500

    Update Tessie.java
    Added all possible rotations to the notes and expanded on how I'll tackle this problem.

--- Tessie.java
+++ Tessie.java
@@ -2,17 +2,19 @@
 
 public Class Tessie {
 
-    /*First we will take in the dimensions. The instructions state to assume
+    /*First I will take in the dimensions. The instructions state to assume
     the structure we are working with is a square. Therefore, we'll just ask
     for one number and assume both dimensions are that number.*/
 
-    /*Second we will take in the god awful string of characters and praise Cthulhu.
+    /*Second I will take in the god awful string of characters and praise Cthulhu.
     Since we can iterate through strings we will based on the number given to us.*/
 
-    /*Third we will use a switch case for rotation because there are four possible
-    rotations according to the instructions. No need to include Satan worship. However,
-    ultimately all this will do is alter how we iterate through the array. Usually,
-    you just go left to right, but we'll gown down-up, left-right, and stuff too.*/
+    /*Third, even though the instructions mention four possible rotations: -90, 0, 90, 180,
+    I will attempt to include every possible rotation from -270 to 270 in increments of 90. 
+    -270, -180, -90, 0, 90, 180, 270. Since any rotation equal to or greater than 360 would
+    still ultimately end up being 90, 180, or a combination thereof, positive or negative, 
+    this would consider every possible rotation. I'll probably still use a switch case and
+    do the math before parsing it into the switch case.*/
 
     /* Should be easy. However, this is a WIP. I work two jobs now (one FT, one PT).
     Give me a few days. Code is easier than it looks, far easier. */

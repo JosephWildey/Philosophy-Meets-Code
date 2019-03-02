c2a3acacfcb9c8461c02343a07a33e2b962425aa
Joseph Wildey <joewildey@outlook.com>
2019-03-02 02:06:57 -0500

    Create Tessie.java

---
+++ Tessie.java
@@ -0,0 +1,19 @@
+//My answer for the Tessellation and Tilings Reddit challenge, when I get around to it.
+
+public Class Tessie {
+
+    /*First we will take in the dimensions. The instructions state to assume
+    the structure we are working with is a square. Therefore, we'll just ask
+    for one number and assume both dimensions are that number.*/
+
+    /*Second we will take in the god awful string of characters and praise Cthulhu.
+    Since we can iterate through strings we will based on the number given to us.*/
+
+    /*Third we will use a switch case for rotation because there are four possible
+    rotations according to the instructions. No need to include Satan worship. However,
+    ultimately all this will do is alter how we iterate through the array. Usually,
+    you just go left to right, but we'll gown down-up, left-right, and stuff too.*/
+
+    /* Should be easy. However, this is a WIP. I work two jobs now (one FT, one PT).
+    Give me a few days. Code is easier than it looks, far easier. */
+}

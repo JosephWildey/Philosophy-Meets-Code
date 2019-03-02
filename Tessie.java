3f8741307979e04657f8f773dbf9cb03e415e3ce
Joseph Wildey <joewildey@outlook.com>
2019-03-02 02:17:35 -0500

    Update Tessie.java

--- Tessie.java
+++ Tessie.java
@@ -14,7 +14,7 @@ public Class Tessie {
     -270, -180, -90, 0, 90, 180, 270. Since any rotation equal to 360 is also equal to 0, and any rotation greater 
     than 360 would still ultimately end up being 90, 180, or a combination thereof, positive or negative, 
     this would consider every possible rotation. I'll probably still use a switch case and
-    do the math before parsing it into the switch case.*/
+    do the math before passing it into the switch case.*/
 
     /* Should be easy. However, this is a WIP. I work two jobs now (one FT, one PT).
     Give me a few days. Code is easier than it looks, far easier. */
